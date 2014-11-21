ef9677d789cb8035737d89c99b7aef7070e381e2
schoenymjaj <mns@productivityedge.com>
2014-11-21 17:15:57 -0500

    fix for ipad keyboard issue
    fix for ipad keyboard issue

--- js/appScript.js
+++ js/appScript.js
@@ -108,6 +108,12 @@ $(function () {
 
             }); //$(document).on
 
+            $(document).on('blur', 'input, textarea', function () {
+                setTimeout(function () {
+                    window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
+                }, 0);
+            });
+
             //sets the padding when window is resized. Not going to happen on a phone.
             $(window).resize(function ()
             {
@@ -1102,28 +1108,26 @@ $(function () {
         app.GameResumeAction = function (index) {
             console.log('func app.GameResumeAction');
 
-            alert('MNS Debug 11/21/14 - GameResumeAction');
-            //  MNS DEBUG 11/21/14 425pm COMMENTED OUT BELOW
-            ////get the game from the queue first
-            //gamePlayQueueBeforPush = app.GetGamePlayQueueLocalStorage();
-            //gamePlayDataBeforePush = gamePlayQueueBeforPush[index].GamePlay;
-            //resultBeforePush = gamePlayQueueBeforPush[index].Result;
+            //get the game from the queue first
+            gamePlayQueueBeforPush = app.GetGamePlayQueueLocalStorage();
+            gamePlayDataBeforePush = gamePlayQueueBeforPush[index].GamePlay;
+            resultBeforePush = gamePlayQueueBeforPush[index].Result;
 
-            ////if game is in progess, then we want to push active game onto the queue
-            //if (app.IsGameInProgress()) {
-            //    app.PushQueueGamePlays(GameState.Active);
-            //}
+            //if game is in progess, then we want to push active game onto the queue
+            if (app.IsGameInProgress()) {
+                app.PushQueueGamePlays(GameState.Active);
+            }
 
-            //app.PutGamePlayLocalStorage(gamePlayDataBeforePush);
-            //app.PutResultLocalStorage(resultBeforePush);
+            app.PutGamePlayLocalStorage(gamePlayDataBeforePush);
+            app.PutResultLocalStorage(resultBeforePush);
 
-            //try {
-            //    app.GetGamePlayStatusServer(resultBeforePush.GameCode);
-            //    $.mobile.loading('hide'); //to show the spinner
-            //} catch (err) {
-            //    $.mobile.loading('hide'); //to show the spinner
-            //    app.popUpHelper("Error", "GetGamePlayStatusServer: " + err);
-            //}
+            try {
+                app.GetGamePlayStatusServer(resultBeforePush.GameCode);
+                $.mobile.loading('hide'); //to show the spinner
+            } catch (err) {
+                $.mobile.loading('hide'); //to show the spinner
+                app.popUpHelper("Error", "GetGamePlayStatusServer: " + err);
+            }
 
             //set the home page for read-only view
             gameState = GameState.ReadOnly;
